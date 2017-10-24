3cb60a6a745374a5b0ddd28b0851b0717c522595
Saulo Pereira <saulo.sobrinho@gprt.ufpe.br>
2017-10-24 18:14:09 -0300

    P2: Início da implementação do projeto

---
+++ projeto2/emotion_recognition.m
@@ -0,0 +1,23 @@
+%=============================================================
+% IN 0996 - Projeto 2
+% Paper: 
+% 
+% Dataset : http://www.kasrl.org/jaffe.html
+%
+% TODO:
+% - Usar transformada de gabor para gerar N imagens partindo 
+% de uma
+% - Implementar dinâmica da programação genética.
+%     Usar apenas índices de operadores.
+% - Testar SVM (1 iteração da evolução)
+% 
+%
+%
+% Author: Saulo P.
+% Date created: 23/10/2017
+%=============================================================
+
+clear all;
+close all;
+
+
---
+++ projeto2/genetic_program.m
@@ -0,0 +1,20 @@
+%=============================================================
+% Genetic Program
+%
+% http://geneticprogramming.com/tutorial/
+%
+% Author: Saulo P.
+% Date created: 23/10/2017
+%=============================================================
+
+op_list = 1:40;
+
+% 0: 1 image  - 1 scalar
+% 1: 1 image  - 1 image
+% 2: 2 images - 1 image
+% 3: 1 image  - 1 vector
+op_arity = [] 
+
+% 1. Random initial population individual computer programs
+
+
